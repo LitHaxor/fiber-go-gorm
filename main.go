a2f8675b0771bc24500b547615792035a5849c8d
Hasan Ahamed <officials.hasan@gmail.com>
2021-12-12 13:47:44 +0600

    3. database connection success

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/LitHaxor/fiber-go-gorm.git/database"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -9,11 +10,12 @@ type Data struct {
     age uint8 `json:"age"`
 }
 
+
 func main() {
     // Start a new fiber app
     app := fiber.New()
 
-   
+   database.ConnectDB()
     // Send a string back for GET calls to the endpoint "/"
     app.Get("/", func(c *fiber.Ctx) error {
         data := []Data{{
